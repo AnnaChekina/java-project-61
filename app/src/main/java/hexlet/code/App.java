523116824a21c72bc76e87922ecb81db5138f667
Anna Chekina <an-na-di@yandex.ru>
2025-04-28 20:40:06 +0300

    feat: add Progression game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import hexlet.code.games.Even;
 import hexlet.code.games.Calc;
 import hexlet.code.games.GCD;
+import hexlet.code.games.Progression;
 
 import java.util.Scanner;
 
@@ -17,6 +18,7 @@ public class App {
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
         System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
         System.out.println("0 - Exit");
 
         checkNumMenu();
@@ -40,6 +42,9 @@ public class App {
                 break;
             case "4":
                 GCD.gcdGame();
+                break;
+            case "5":
+                Progression.progressionGame();
                 break;
             case "0":
                 System.out.println("Exiting the game. Goodbye!");
---
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -0,0 +1,51 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+import java.util.Random;
+
+public final class Progression {
+
+    private Progression() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    private static final Random RANDOM = new Random();
+    private static final int MAX_RANDOM_FIRST_NUMBER = 20;
+    private static final int MAX_RANDOM_STEP = 10;
+    private static final int MAX_RANDOM_POSITION = 10;
+
+    static String[] generateRoundData() {
+        int randomFirstNumber = RANDOM.nextInt(MAX_RANDOM_FIRST_NUMBER) + 1;
+        int randomStep = RANDOM.nextInt(MAX_RANDOM_STEP) + 1;
+        int randomPosition = RANDOM.nextInt(MAX_RANDOM_POSITION);
+        int nextNumber = randomFirstNumber;
+        int corAnswer = 0;
+        StringBuilder question = new StringBuilder();
+
+        for (var i = 0; i < MAX_RANDOM_POSITION; i++) {
+            if (i == randomPosition) {
+                question.append(".. ");
+                corAnswer = nextNumber;
+            } else {
+                question.append(nextNumber).append(" ");
+            }
+            nextNumber += randomStep;
+        }
+        return new String[] {String.valueOf(question).trim(), Integer.toString(corAnswer)};
+    }
+
+    public static void progressionGame() {
+        String gameConditions = "What number is missing in the progression?";
+        String[] questions = new String[3];
+        String[] corAnswers = new String[3];
+
+        for (var i = 0; i < 3; i++) {
+            String[] roundData = generateRoundData();
+
+            questions[i] = roundData[0];
+            corAnswers[i] = roundData[1];
+        }
+
+        Engine.start(gameConditions, questions, corAnswers);
+    }
+}
