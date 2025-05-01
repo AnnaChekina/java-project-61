29aa90e840fcecf4d019f21044f4a577e8296f55
Anna Chekina <an-na-di@yandex.ru>
2025-05-01 22:55:45 +0300

    feat: add Prime game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,6 +4,7 @@ import hexlet.code.games.Even;
 import hexlet.code.games.Calc;
 import hexlet.code.games.GCD;
 import hexlet.code.games.Progression;
+import hexlet.code.games.Prime;
 
 import java.util.Scanner;
 
@@ -19,6 +20,7 @@ public class App {
         System.out.println("3 - Calc");
         System.out.println("4 - GCD");
         System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
         System.out.println("0 - Exit");
 
         checkNumMenu();
@@ -45,6 +47,9 @@ public class App {
                 break;
             case "5":
                 Progression.progressionGame();
+                break;
+            case "6":
+                Prime.primeGame();
                 break;
             case "0":
                 System.out.println("Exiting the game. Goodbye!");
---
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -0,0 +1,54 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+import java.util.Random;
+
+public final class Prime {
+
+    private Prime() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    private static final Random RANDOM = new Random();
+    private static final int MAX_RANDOM_NUMBER = 100;
+
+    static int getRandomNumber() {
+        return RANDOM.nextInt(MAX_RANDOM_NUMBER) + 1;
+    }
+
+    static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void primeGame() {
+        String gameConditions = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+        String[] questions = new String[3];
+        String[] corAnswers = new String[3];
+
+        int randomNumber;
+        String corAnswer;
+
+        for (var i = 0; i < 3; i++) {
+            randomNumber = getRandomNumber();
+
+            if (isPrime(randomNumber)) {
+                corAnswer = "yes";
+            } else {
+                corAnswer = "no";
+            }
+
+            questions[i] = Integer.toString(randomNumber);
+            corAnswers[i] = corAnswer;
+        }
+
+        Engine.start(gameConditions, questions, corAnswers);
+    }
+}
